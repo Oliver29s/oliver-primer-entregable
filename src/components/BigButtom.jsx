8052b549e6f87b3a37b49ba591468d342654e711
Oliver_rodriguez29 <mawiis455@gmail.com>
2023-01-24 13:31:53 -0500

    siuuu

--- src/components/BigButtom.jsx
+++ src/components/BigButtom.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const BigButtom = ({ClickHand,colorRamdon}) => {
   return (
-    <div >
+   
         <button className='shadow__btn' style={{ background: colorRamdon}}  onClick={ClickHand}>change color</button>
-    </div>
+   
   )
 }
 
